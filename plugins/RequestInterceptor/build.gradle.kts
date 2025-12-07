8269622272a4e79ff9ddc170ecb1c0003a38a397
canny1913 <canny1913@outlook.com>
2025-12-07 20:16:45 +0300

    feat: wip request interceptor plugin

---
+++ plugins/RequestInterceptor/build.gradle.kts
@@ -0,0 +1,11 @@
+version = "0.0.0" // Plugin version. Increment this to trigger an update
+description = "Adds ability to intercept Rest API requests/responses." // Plugin description that will be shown to user
+
+aliucord {
+    // Changelog of your plugin
+    changelog.set(
+        """
+        """.trimIndent()
+    )
+    deploy.set(false)
+}
