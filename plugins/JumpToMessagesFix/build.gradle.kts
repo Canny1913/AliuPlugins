e8c29e70218fe6cba8cfb628e2e7ae2763476d6c
canny1913 <canny1913@outlook.com>
2025-12-08 20:55:59 +0300

    chore: remove example comments

--- plugins/JumpToMessagesFix/build.gradle.kts
+++ plugins/JumpToMessagesFix/build.gradle.kts
@@ -1,8 +1,7 @@
-version = "0.0.1" // Plugin version. Increment this to trigger an update
-description = "Fixes message links not jumping to correct message." // Plugin description that will be shown to user
+version = "0.0.1"
+description = "Fixes message links not jumping to correct message."
 
 aliucord {
-    // Changelog of your plugin
     changelog.set(
         """
         # 0.0.1
