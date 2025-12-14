3c7b26439b04ef6ab3d8373ffb868e1f9ef3dc7b
canny1913 <canny1913@outlook.com>
2025-12-14 14:20:03 +0300

    chore: bump version

--- plugins/JumpToMessagesFix/build.gradle.kts
+++ plugins/JumpToMessagesFix/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "0.0.3"
+version = "0.0.4"
 description = "Fixes message links not jumping to correct message."
 
 aliucord {
     changelog.set(
         """
+        # 0.0.4
+        * Removed leftover code that was causing bugs
+        
         # 0.0.3
         * Attempts to fix inconsistencies yet again.
 
