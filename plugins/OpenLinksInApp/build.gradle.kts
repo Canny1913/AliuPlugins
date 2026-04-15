a13f8926797b8bd217a42538e9468e7d5b80b783
canny1913 <canny1913@outlook.com>
2026-04-15 21:03:15 +0300

    feat: add open links in app plugin with 1.0.1 update

---
+++ plugins/OpenLinksInApp/build.gradle.kts
@@ -0,0 +1,15 @@
+version = "1.0.1"
+description = "Forces URL handler to open links in current app."
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.1
+        * Fixes canary/ptb links.
+        
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    deploy.set(true)
+}
