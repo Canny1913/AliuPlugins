6e5ce308bcb518deb1b1fcb4c4f8d543fdfa7724
canny1913 <canny1913@outlook.com>
2026-07-20 23:54:29 +0300

    feat: ghosted

---
+++ plugins/Ghosted/build.gradle.kts
@@ -0,0 +1,12 @@
+version = "0.0.1"
+description = "Displays a ghost icon upon ghosting DMs"
+
+aliucord {
+    changelog.set(
+        """
+        # 0.0.1
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    deploy.set(true)
+}
