601f0413192336f192c10c51e267f7e5b4e3e6b5
canny1913 <canny1913@outlook.com>
2026-07-21 19:30:03 +0300

    chore: bump ghosted to 0.0.2

--- plugins/Ghosted/build.gradle.kts
+++ plugins/Ghosted/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "0.0.1"
+version = "0.0.2"
 description = "Displays a ghost icon upon ghosting DMs"
 
 aliucord {
     changelog.set(
         """
+        # 0.0.2
+        * Fix crash due to bad build config.
+        
         # 0.0.1
         * Initial plugin release.
         """.trimIndent()
